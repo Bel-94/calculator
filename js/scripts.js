6d8b540fc5e69622962dffb76ad3632186032f65
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-03-09 02:19:10 +0300

    add changes for subtract, divide, remainder, & multiply

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,43 @@ alert(subtract(10, 7));
  var number2 = prompt("Enter another number:");
  alert(add(number1, number2)); */
  
- var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  alert(add(number1, number2));
+//  var add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = (add(number1, number2));
+//   alert (result);
+
+
+//   var subtract = function(number1, number2) {
+//     return number1 - number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = (subtract(number1, number2));
+//   alert (result);
+
+//   var multiply = function(number1, number2) {
+//     return number1 * number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = (multiply(number1, number2));
+//   alert (result);
+
+//   var divide = function(number1, number2) {
+//     return number1 / number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = (divide(number1, number2));
+//   alert (result);
+
+// var remainder = function(number1, number2) {
+//     return number1 % number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = (remainder(number1, number2));
+//   alert (result);
